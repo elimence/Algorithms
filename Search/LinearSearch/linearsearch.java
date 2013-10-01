b6d1c455b58f21624b68033bd5b01a212ce204c0
Samuel A <elimence@gmail.com>
2013-10-01 00:46:42 +0000

    initial commit for linear search algorithm

---
+++ Search/LinearSearch/linearsearch.java
@@ -0,0 +1,20 @@
+
+public class linearsearch {
+	private int[] data;
+
+	public linearsearch(data) {
+
+	}
+
+	public lin_search_rec() {
+
+	}
+
+	public lin_search_for() {
+
+	}
+
+	public lin_search_bac() {
+		
+	}
+}
